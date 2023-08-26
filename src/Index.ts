a15786f958c6d6f595bbc54b61e6bb53ba3d34ad
shankar <shankargiri.Sl2000@gmail.com>
2023-08-26 08:42:29 +0530

    typeScript Demo

---
+++ src/Index.ts
@@ -0,0 +1,15 @@
+// var username:string = "John";
+//document.write("Hello ! " + username);
+// console.log("Hello ! " + username)
+
+function Demo()
+{
+    let x:number;
+    x = 10;
+    if(x==10)
+    {
+        let y:number = 20;
+        console.log("x = " + x + "\n" + "y = " + y);
+    }
+}
+Demo();
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// var username:string = "John";
+//document.write("Hello ! " + username);
+// console.log("Hello ! " + username)
+function Demo() {
+    var x;
+    x = 10;
+    if (x == 10) {
+        var y = 20;
+        console.log("x = " + x + "\n" + "y = " + y);
+    }
+}
+Demo();
