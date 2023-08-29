145af31656474c3082a47f1788dadbeec4a0f569
shankar <shankargiri.Sl2000@gmail.com>
2023-08-29 09:31:49 +0530

    Map Type & Date type

---
+++ src/maptype.js
@@ -0,0 +1,7 @@
+//MAP TYPE
+// let data:Map<any, string> = new Map();
+// data.set(1, "HTML is a ,arkup language");
+// data.set("CSS", "It is used to define style");
+//DATE TYPE
+var Mfd = new Date("2023-01-02 20:20:32.89");
+console.log(Mfd.toLocaleDateString());
---
+++ src/maptype.ts
@@ -0,0 +1,8 @@
+//MAP TYPE
+// let data:Map<any, string> = new Map();
+// data.set(1, "HTML is a ,arkup language");
+// data.set("CSS", "It is used to define style");
+
+//DATE TYPE
+let Mfd:Date = new Date("2023-01-02 20:20:32.89")
+console.log(Mfd.toLocaleDateString());
