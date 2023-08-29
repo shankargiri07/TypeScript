2d854f2a92474d12df6895df2d198db235b113c2
shankar <shankargiri.Sl2000@gmail.com>
2023-08-29 08:20:27 +0530

    object type

---
+++ src/objecttype.js
@@ -0,0 +1,5 @@
+var tv = {
+    Name: "Samsung TV",
+    Price: 4500.22,
+};
+console.log("Name:".concat(tv.Name, "\nPrice:").concat(tv.Price));
---
+++ src/objecttype.ts
@@ -0,0 +1,6 @@
+let tv:{Name:string, Price:number, Stock?:boolean} = {
+    Name : "Samsung TV",
+    Price :4500.22,
+}
+console.log(`Name:${tv.Name}\nPrice:${tv.Price}`);
+
